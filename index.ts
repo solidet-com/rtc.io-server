841c8a983690446a314e147eb95217fec308e014
Fudayl Cavus <fudaylcavus@gmail.com>
2024-01-14 21:18:42 +0300

    code format

--- index.ts
+++ index.ts
@@ -1,28 +1,33 @@
-import { Server as RootServer, Socket, DisconnectReason, ServerOptions as RootServerOptions, Namespace, BroadcastOperator, RemoteSocket, Event } from "socket.io";
+import {
+    Server as RootServer,
+    Socket,
+    DisconnectReason,
+    ServerOptions as RootServerOptions,
+    Namespace,
+    BroadcastOperator,
+    RemoteSocket,
+    Event,
+} from "socket.io";
 import { createServer, IncomingMessage, ServerResponse } from "http"; // Import the 'http' module
 import { addDefaultListeners } from "./lib/defaulthandlers";
-import { whipManager } from './lib/whip-whep';
+import { whipManager } from "./lib/whip-whep";
 
 interface ServerOptions extends RootServerOptions {}
 
 export class Server extends RootServer {
-
     constructor(opts?: Partial<ServerOptions>) {
         super(opts);
-
-
     }
 
     listen(port: number, opts?: Partial<ServerOptions>): this {
         console.log("extended listen method");
 
-const options = {
-    rtcHttpServerPort: port || 3000,
-    socketIoServerOptions: {
-    }
-};
+        const options = {
+            rtcHttpServerPort: port || 3000,
+            socketIoServerOptions: {},
+        };
 
-const server = whipManager(options);
+        const server = whipManager(options);
 
         return this;
     }

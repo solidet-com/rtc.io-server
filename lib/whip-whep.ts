b2c4e6780560f58dcd784542670665232c39922f
Fudayl Cavus <fudaylcavus@gmail.com>
2024-01-15 19:25:44 +0300

    chore: new iceServer

--- lib/whip-whep.ts
+++ lib/whip-whep.ts
@@ -33,7 +33,7 @@ export function whipManager(options: WhipManagerOptions = {}) {
         const servers = {
             iceServers: [
                 {
-                    urls: ["stun:stun.l.google.com:19302"],
+                    urls: ["stun:stun1.l.google.com:19302", "stun:stun2.l.google.com:19302"],
                 },
             ],
         };

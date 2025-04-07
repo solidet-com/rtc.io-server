cb7b6ad3a757bba9fa345bb15f1266132523210e
trsnaqe <trsnaqe@gmail.com>
2025-04-07 22:06:03 +0300

    add rtc-message handler

--- lib/defaulthandlers.ts
+++ lib/defaulthandlers.ts
@@ -13,10 +13,15 @@ function rtcCandiateHandler(socket: Socket, data: MessagePayload<RTCIceCandidate
     socket.to(data.target).emit("#candidate", data);
 }
 
+function rtcMessageHandler(socket: Socket, data: MessagePayload<string>) {
+    socket.to(data.target).emit("#rtc-message", data);
+}
+
 function addDefaultListeners(socket: Socket) {
     socket.on("#offer", (data) => rtcOfferHandler(socket, data));
     socket.on("#answer", (data) => rtcAnswerHandler(socket, data));
     socket.on("#candidate", (data) => rtcCandiateHandler(socket, data));
+    socket.on("#rtc-message", (data) => rtcMessageHandler(socket, data));
 }
 
 export { addDefaultListeners };
